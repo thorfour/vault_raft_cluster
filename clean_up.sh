03ea1d87b443c420903c42b0e684eb243898575a
Thor Hansen <me@thor-hansen.com>
2020-05-11 08:42:02 -0500

    cleanup ignore errors

--- clean_up.sh
+++ clean_up.sh
@@ -1,9 +1,9 @@
-rm -rf certs
-rm config*.hcl
+rm -rf certs 2> /dev/null
+rm config*.hcl 2> /dev/null 
 
 for ((i=0; i < $1; i++))
 do
-    docker stop vault$i
+    docker stop vault$i 2> /dev/null
 done
 
-docker network rm vault
+docker network rm vault 2> /dev/null
