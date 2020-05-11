d0873fb3dae9d69d60b1d65f5f3e34c9078902ea
Thor Hansen <me@thor-hansen.com>
2020-05-11 14:59:18 -0500

    unseal all instances

--- vault_cluster.sh
+++ vault_cluster.sh
@@ -5,20 +5,20 @@ clusterSize="${cluster_size:-$1}"
 basePort=8200
 
 generate_ca() {
-    openssl genrsa -out myCA.key 2048
+    openssl genrsa -out myCA.key 2048 &> /dev/null
 }
 
 generate_ca_crt() {
-    openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress=" 2> /dev/null
+    openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress=" &> /dev/null
 }
 
 generate_key() {
-    openssl genrsa -out vault$1.key 2048
+    openssl genrsa -out vault$1.key 2048 &> /dev/null
     chmod 664 vault$1.key
 }
 
 generate_csr() {
-    openssl req -new -key vault$1.key -out vault$1.csr -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress=" 2> /dev/null
+    openssl req -new -key vault$1.key -out vault$1.csr -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress=" &> /dev/null 
 }
 
 sign_crt() {
@@ -36,7 +36,7 @@ IP.2 = 127.0.0.1
 EOF
 
 openssl x509 -req -in vault$1.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial \
-    -out vault$1.crt -days 1825 -sha256 -extfile vault$1.ext
+    -out vault$1.crt -days 1825 -sha256 -extfile vault$1.ext &> /dev/null
 }
 
 generate_all_certs() {
@@ -132,8 +132,20 @@ init_response=$(VAULT_CACERT=certs/myCA.pem vault operator init -format=json -ke
 key=$(echo $init_response | jq -r .unseal_keys_b64[0])
 token=$(echo $init_response | jq -r .root_token)
 
+echo ""
+echo "======================================================"
 echo "Key: $key"
 echo "Root Token: $token"
+echo "======================================================"
+echo ""
+
+# Unseal remainder of Vault instances
+for ((j = 0; j < $clusterSize; j++))
+do
+    let port=$basePort+$j*2
+    VAULT_CACERT=certs/myCA.pem vault operator unseal -address=https://localhost:$port $key 1> /dev/null
+    sleep 15
+done
 }
 
 main
